dc78e1bd702423973d0ff6656a1baf54dda7a637
Adeboye Tolulope Ibukun <78228969+tolutbless@users.noreply.github.com>
2023-11-24 05:35:34 +0100

    Update with for_each main.tf
    Use for_each instead of count and convert the list to set.

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "this" {
 
 
 resource "azurerm_storage_account" "storage" {
-  count                    = length(var.account_names)
-  name                     = var.account_names[count.index]
+  for_each                 = toset(var.account_names)
+  name                     = each.value
   resource_group_name      = azurerm_resource_group.this.name
   location                 = azurerm_resource_group.this.location
   account_replication_type = "LRS"
